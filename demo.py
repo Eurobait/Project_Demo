4260c75c5e0f8b967439badb0658699160c426ef
Ken Lei <dingkunl@andrew.cmu.edu>
2022-04-26 18:53:39 -0400

    Remove the prediction module

--- demo.py
+++ demo.py
@@ -20,38 +20,16 @@ np.random.seed(87)
 
 
 '''
-# Load Model
-class AE(nn.Module):
-    def __init__(self):
-        super(AE, self).__init__()
-        self.encoder = nn.Sequential(
-            nn.Linear(input_dim, encoding_dim),
-            nn.Tanh(),
-            nn.Linear(encoding_dim, hidden_dim),
-            nn.LeakyReLU(),
-            )
-        self.decoder = nn.Sequential(
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.Tanh(),
-            nn.Linear(hidden_dim, input_dim),
-            nn.LeakyReLU()
-            )
-    def forward(self , x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return(x)
-
-device = torch.device("cpu")
-scaler = joblib.load('scaler.gz')
-PATH = "current_model.pt"
-model = torch.load(PATH)
-model.eval()
+## Load Threshold
 '''
 
-
-# Load Threshold
+## Load Threshold
 test_summary = pd.read_csv("test_results.csv")
 
+
+'''
+## Display precision recall curve
+'''
 ## Display precision recall curve
 '''
 PrecisionRecallDisplay.from_predictions(test_summary.Class, test_summary.pred_loss)
@@ -59,14 +37,13 @@ plt.ylim((0,1))
 plt.show()
 '''
 
-
-
-# Collect user preference
+'''
+## Enter your performance preference
+'''
+## Collect user preference
 u_precision = st.number_input("Expected Precision", 0.00)
 u_recall = st.number_input("Expected Recall", 0.00)
 
-
-# Add a line to verify that only one of the input precision / recall is not 0
 
 
 
@@ -101,6 +78,9 @@ else:
     threshold, precision, recall = nan, nan, nan
 
 
+'''
+## Expected performance summary
+'''
 
 # output the expected values
 st.write("The current threshold is",  threshold)
@@ -108,42 +88,6 @@ st.write("The current expected precision is",  precision)
 st.write("The current expected recall is",  recall)
 
 
-
-'''
-# Prediction
-# read a small dataset for demo, there should not be any label
-# Label is included here for revision reference only
-batch_test = pd.read_csv("demo_test.csv") 
-batch_id = batch_test["Flow_ID"]
-batch_timestamp = batch_test["Time_stamp"]
-
-batch_X = batch_test.iloc[:,2:86]
-
-batch_loader = DataLoader(batch_X, batch_size = 2048, shuffle=False)
-
-
-criterion = nn.MSELoss(reduction='none')
-
-with torch.no_grad():
-        model.eval() 
-        anomaly_scores = []
-
-        for X in batch_loader:
-            X = X.to(device)
-            outputs = model(X)
-            loss = criterion(outputs, X).detach().cpu().numpy()
-            anomaly_scores.append(loss)
-
-            del X
-            del outputs
-            del loss
-
-anomaly_scores = np.sum(np.concatenate(anomaly_scores, axis=0), axis=1)
-
-errorDF = pd.DataFrame(anomaly_scores, columns=["pred_loss"])
-errorDF['batch_id'] = batch_id
-errorDF['Time_stamp'] = batch_timestamp
-'''
 
 # Modify the labels
 
