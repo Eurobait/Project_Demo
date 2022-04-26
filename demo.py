6c991d3e16048b6b8c95edc50b0704fe0e26fd23
Ken Lei <dingkunl@andrew.cmu.edu>
2022-04-26 14:14:40 -0400

    Remove the plots to test other parts

--- demo.py
+++ demo.py
@@ -52,22 +52,51 @@ model.eval()
 test_summary = pd.read_csv("test_results.csv")
 
 ## Display precision recall curve
-# fig = PrecisionRecallDisplay.from_predictions(test_summary.Class, test_summary.pred_loss)
-# st.pyplot(fig)
-
-precision, recall, th = precision_recall_curve(
-    test_summary.Class, 
-    test_summary.pred_loss)
-st.plot(recall, precision, 'b', label='Precision-Recall curve')
+'''
+PrecisionRecallDisplay.from_predictions(test_summary.Class, test_summary.pred_loss)
+plt.ylim((0,1))
+plt.show()
+'''
 
 
 
+# Collect user preference
+u_precision = st.number_input("Expected Precision", 0.00)
+u_recall = st.number_input("Expected Recall", 0.00)
 
 
-mold = st.number_input("Chance of mold", 0.01)
+# Add a line to verify that only one of the input precision / recall is not 0
 
 
 
+# Search for threshold based on precision
+def threshold_by_precision(p):
+    # we need to exclude first 1000 rows in the search process
+    df = test_summary.iloc[1000:,:]
+    df["Precision"] = abs(df["Precision"] - u_precision)
+    df = df.sort_values(by=['Precision'], ascending=True)
+    threshold, type, precision, recall = test_summary.iloc[df.index[0], :]
+    return threshold, precision, recall
 
 
-st.write("Just Harvest")
+# Search for threshold based on precision
+def threshold_by_recall(p):
+    # we need to exclude first 1000 rows in the search process
+    df = test_summary.iloc[1000:,:]
+    df["Recall"] = abs(df["Recall"] - u_recall)
+    df = df.sort_values(by=['Recall'], ascending=True)
+    threshold, type, precision, recall = test_summary.iloc[df.index[0], :]
+    return threshold, precision, recall
+
+
+if u_precision == 0.00 & u_recall != 0.00:
+    threshold, precision, recall = threshold_by_recall(u_recall)
+elif u_precision != 0.00 & u_recall == 0.00:
+    threshold, precision, recall = threshold_by_precision(u_precision)
+else: st.write("Input is not valid. Only one metric can be non-zero to initiate a search" )
+
+
+
+st.write("The current threshold is",  threshold)
+st.write("The current expected precision is",  precision)
+st.write("The current expected recall is",  recall)
